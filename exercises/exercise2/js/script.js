b52b6e2403f4a6ac76c32403f0ea003298ac4237
Harout77 <harout@live.ca>
2018-09-26 15:11:52 -0400

    changed the enemy into asteroids
    i removed all the codes relating to the enemy and replaced it with asteroid and also removed the size increase

--- exercises/exercise2/js/script.js
+++ exercises/exercise2/js/script.js
@@ -16,31 +16,24 @@ var avatarSpeed = 10;
 var avatarVX = 0;
 var avatarVY = 0;
 
-// The position and size of the enemy circle
-var enemyX;
-var enemyY;
-var enemySize = 50;
-// How much bigger the enemy circle gets with each successful dodge
-var enemySizeIncrease = 5;
-
-// The speed and velocity of our enemy circle
-var enemySpeed = 5;
-var enemyVX = 5;
-// How much bigger the enemy circle gets with each successful dodge
-var enemyrease = 0.5;
 
 // How many dodges the player has made
 var dodges = 0;
 
+//background image
 var space;
 var y;
 
+// The position and size of the astroids
 var asteroid;
 var asteroidX;
 var asteroidY;
-var asteroidspeed = 5;
+var asteroidSize = 50;
 
-
+// asteroid speed and velocity
+var asteroidSpeed = 5;
+var asteroidVX = 0;
+var asteroidVY = 0;
 
 
 // setup()
@@ -49,6 +42,8 @@ var asteroidspeed = 5;
 function setup() {
   // Create our playing area
   createCanvas(750,450);
+
+  // Loading assets
   space = loadImage("assets/space.jpg");
   asteroid = loadImage("assets/asteroid.png");
 
@@ -56,14 +51,10 @@ function setup() {
   avatarX = width/2;
   avatarY = height/2;
 
-  // Put the enemy to the left at a random y coordinate within the canvas
-  enemyX = 0;
-  enemyY = random(0,height);
 
-  // No stroke so it looks cleaner
-  noStroke();
-    image(space, 0, 0);
-
+  // Put asteroid at random position
+  asteroidX = 0;
+  asteroidY = random(0,height);
 }
 
 // draw()
@@ -71,9 +62,8 @@ function setup() {
 // Handle moving the avatar and enemy and checking for dodges and
 // game over situations.
 function draw() {
-  // A pink background
 
-
+  // placing the background image
 
   background(space);
 
@@ -112,23 +102,23 @@ function draw() {
   avatarX = avatarX + avatarVX;
   avatarY = avatarY + avatarVY;
 
-  // The enemy always moves at enemySpeed (which increases)
-  enemyVX = enemySpeed;
-  // Update the enemy's position based on its velocity
-  enemyX = enemyX + enemyVX;
 
-  // Check if the enemy and avatar overlap - if they do the player loses
-  // We do this by checking if the distance between the centre of the enemy
+  asteroidVX = asteroidSpeed;
+  asteroidX = asteroidX + asteroidVX;
+
+  // Check if the asteroid and avatar overlap - if they do the player loses
+  // We do this by checking if the distance between the centre of the asteroid
   // and the centre of the avatar is less that their combined radii
-  if (dist(enemyX,enemyY,avatarX,avatarY) < enemySize/2 + avatarSize/2) {
+
+  if (dist(asteroidX,asteroidY,avatarX,avatarY) < asteroidSize/2 + avatarSize/2) {
     // Tell the player they lost
     console.log("YOU LOSE!");
     // Reset the enemy's position
-    enemyX = 0;
-    enemyY = random(0,height);
+    asteroidX = 0;
+    asteroidY = random(0,height);
     // Reset the enemy's size and speed
-    enemySize = 50;
-    enemySpeed = 5;
+    asteroidSize = 50;
+    asteroidSpeed = 5;
     // Reset the avatar's position
     avatarX = width/2;
     avatarY = height/2;
@@ -140,43 +130,36 @@ function draw() {
   if (avatarX < 0 || avatarX > width || avatarY < 0 || avatarY > height) {
     // If they went off the screen they lose in the same way as above.
     console.log("YOU LOSE!");
-    enemyX = 0;
-    enemyY = random(0,height);
-    enemySize = 50;
-    enemySpeed = 5;
+    asteroidX = 0;
+    asteroidY = random(0,height);
+    asteroidSize = 50;
+    asteroidSpeed = 5;
     avatarX = width/2;
     avatarY = height/2;
     dodges = 0;
   }
 
   // Check if the enemy has moved all the way across the screen
-  if (enemyX > width) {
+  if (asteroidX > width) {
     // This means the player dodged so update its dodge statistic
     dodges = dodges + 1;
     // Tell them how many dodges they have made
-    console.log(dodges + " DODGES!");
     // Reset the enemy's position to the left at a random height
-    enemyX = 0;
-    enemyY = random(0,height);
+    asteroidX = 0;
+    asteroidY = random(0,height);
     // Increase the enemy's speed and size to make the game harder
-    enemySpeed = enemySpeed + enemyrease;
-    enemySize = enemySize + enemySizeIncrease;
   }
+  // // Display the current number of successful in the console
+  // console.log(dodges);
 
-  // Display the current number of successful in the console
-  console.log(dodges);
 
   // The player is black
   fill(0);
   // // Draw the player as a circle
   ellipse(avatarX,avatarY,avatarSize,avatarSize);
   //
-  // // The enemy is red
-  fill(255,0,0);
-  // // // Draw the enemy as a circle
-  ellipse(enemyX,enemyY,enemySize,enemySize);
 
-  image(asteroid, 0, 0);
+
 
  //Display score
   textSize(20);
@@ -185,7 +168,7 @@ function draw() {
 
 
 
-
+image(asteroid,asteroidX,asteroidY);
 
 
 
