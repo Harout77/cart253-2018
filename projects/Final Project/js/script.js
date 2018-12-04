53981d6a7ff2b6ef0d3a63d7338b0e8820bc2a42
Harout77 <harout@live.ca>
2018-12-03 22:21:28 -0500

    FG created a template for game 1
    just changed few colours to see if the next scene is working

--- projects/Final Project/js/script.js
+++ projects/Final Project/js/script.js
@@ -6,6 +6,7 @@ createCanvas(displayWidth,displayHeight);
 mgr = new SceneManager();
 
 mgr.addScene ( Intro );
+mgr.addScene ( Game1 );
 
 mgr.showNextScene();
 
@@ -29,9 +30,9 @@ function keyPressed()
         case '1':
             mgr.showScene( Intro );
             break;
-        // case '2':
-        //     mgr.showScene( game1 );
-        //     break;
+        case '2':
+            mgr.showScene( Game1 );
+            break;
         // case '3':
         //     mgr.showScene( game2 );
         //     break;
@@ -43,14 +44,50 @@ function keyPressed()
 
 
 function Intro () {
-  background("black")
-  fill("red");
-  textAlign(CENTER);
-  textSize(64);
-  text('Master Game Project V 1.0', displayWidth / 2, displayHeight /2.5);
-  fill("yellow");
-  textAlign(CENTER);
-  textSize(36);
-  text('Press any key to begin', displayWidth / 2, displayHeight /2);
+
+this.draw = function()
+  {
+    background("black")
+    fill("red");
+    textAlign(CENTER);
+    textSize(64);
+    text('Master Game Project V 1.0', displayWidth / 2, displayHeight /2.5);
+    fill("yellow");
+    textAlign(CENTER);
+    textSize(36);
+    text('Press any key to begin', displayWidth / 2, displayHeight /2);
+  
+  }
+
+
+
+
+    this.mousePressed = function()
+    {
+        this.sceneManager.showNextScene();
+    }
+
+
+}
+
+function Game1(){
+
+     this.draw = function()
+     {
+       background("green")
+       fill("red");
+       textAlign(CENTER);
+       textSize(64);
+       text('Master Game Project V 1.0', displayWidth / 2, displayHeight /2.5);
+       fill("blue");
+       textAlign(CENTER);
+       textSize(36);
+       text('Press any key to begin', displayWidth / 2, displayHeight /2);
+     }
+
+     this.mousePressed = function()
+     {
+         this.sceneManager.showNextScene();
+     }
 
 }
