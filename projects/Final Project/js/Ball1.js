827307516c7eac2a4fa08775365c999bf8959860
Harout77 <harout@live.ca>
2018-12-06 12:13:20 -0500

    Revert "FP copied the project2 version of pong"
    This reverts commit 62a65145e90f4044decf650dbd2e898392f0bc61.

--- projects/Final Project/js/Ball1.js
+++ projects/Final Project/js/Ball1.js
@@ -15,22 +15,7 @@ function Ball1(x,y,vx,vy,size,speed) {
   this.size = size;
   this.speed = speed;
 }
-function BadBall(x, y, vx, vy, size, speed) {
-  this.x = x;
-  this.y = y;
-  this.vx = vx;
-  this.vy = vy;
-  this.size = size;
-  this.speed = speed;
-}
-var r, g, b;
-var hitsound;
-var losesound;
-function preload() {
-  beepSFX = new Audio("assets/audio/beep.wav");
-  hitsound = new Audio("assets/audio/hit.wav");
-  losesound = new Audio("assets/audio/lose.wav");
-}
+
 // update()
 //
 // Moves according to velocity, constrains y to be on screen,
@@ -47,27 +32,6 @@ Ball1.prototype.update = function () {
   // Check for touching upper or lower edge and reverse velocity if so
   if (this.y === 0 || this.y + this.size === height) {
     this.vy = -this.vy;
-    /// Play beep at each collision
-   beepSFX.currentTime = 0;
-   beepSFX.play();
-  }
-}
-BadBall.prototype.update = function() {
-  // Update position with velocity
-  this.x += this.vx;
-  this.y += this.vy;
-
-  // Constrain y position to be on screen
-  this.y = constrain(this.y, 0, height - this.size);
-
-  // Check for touching upper or lower edge and reverse velocity if so
-  if (this.y === 0 || this.y + this.size === height) {
-    this.vy = -this.vy;
-
-    ///// NEW /////
-    /// Play beep at each collision
-    hitsound.currentTime = 0;
-    hitsound.play();
   }
 }
 
@@ -78,22 +42,13 @@ BadBall.prototype.update = function() {
 Ball1.prototype.isOffScreen = function () {
   // Check for going off screen and reset if so
   if (this.x + this.size < 0 || this.x > width) {
-    losesound.currentTime = 0;
-    losesound.play();
     return true;
   }
   else {
     return false;
   }
 }
-BadBall.prototype.isOffScreen = function() {
-  // Check for going off screen and reset if so
-  if (this.x + this.size < 0 || this.x > width) {
-    return true;
-  } else {
-    return false;
-  }
-}
+
 // display()
 //
 // Draw the Ball1 as a rectangle on the screen
@@ -113,12 +68,9 @@ Ball1.prototype.handleCollision = function(paddle) {
     if (this.y + this.size > paddle.y && this.y < paddle.y + paddle.h) {
       // If so, move Ball1 back to previous position (by subtracting current velocity)
       this.x -= this.vx;
-       this.y -= this.vy;
-       // Reverse x velocity to bounce
-       this.vx = -this.vx;
-       this.vy = -this.vy;
-       beepSFX.currentTime = 0;
-     beepSFX.play();
+      this.y -= this.vy;
+      // Reverse x velocity to bounce
+      this.vx = -this.vx;
     }
   }
 }
@@ -127,50 +79,6 @@ Ball1.prototype.handleCollision = function(paddle) {
 //
 // Set position back to the middle of the screen
 Ball1.prototype.reset = function () {
-  fill(r, g, b);
-  noStroke();
-  rect(this.x, this.y, this.size, this.size);
-}
-Ball1.prototype.reset = function () {
-  fill(r, g, b);
-  noStroke();
-  rect(this.x, this.y, this.size, this.size);
-}
-BadBall.prototype.display = function() {
-  ////// NEW ///////
-  //// Randomize the color of the ball
-  fill(255, 0, 0);
-  noStroke();
-  rect(this.x, this.y, this.size, this.size);
-}
-
-BadBall.prototype.handleCollision = function(paddle) {
-  // Check if the ball overlaps the paddle on x axis
-  if (this.x + this.size > paddle.x && this.x < paddle.x + paddle.w) {
-    // Check if the ball overlaps the paddle on y axis
-    if (this.y + this.size > paddle.y && this.y < paddle.y + paddle.h) {
-      // If so, move ball back to previous position (by subtracting current velocity)
-      this.x -= this.vx;
-      this.y -= this.vy;
-      // Reverse x velocity to bounce
-      this.vx = -this.vx;
-      this.vy = -this.vy;
-      paddle.score-- ;
-      paddle.score = constrain(paddle.score,0,11)
-      ///Play sound at each collision
-      hitsound.currentTime = 0;
-      hitsound.play();
-    }
-  }
-}
-BadBall.prototype.reset = function(winner) {
-  this.x = width / 2;
-  this.y = height / 2;
-  ///// NEW ////
-  this.vy = random(-5, 10);
-  // //  Speed increase with each RESET
-  this.speed = -this.speed;
-  this.vx = -1 * this.vx;
-  // ball gets bigger also;
-  this.size = random(7, 30);
+  this.x = width/2;
+  this.y = height/2;
 }
--- projects/Final Project/js/Paddle1.js
+++ projects/Final Project/js/Paddle1.js
@@ -6,20 +6,16 @@
 // Paddle1 constructor
 //
 // Sets the properties with the provided arguments or defaults
-function Paddle1(x, y, w, h, speed, downKey, upKey, leftKey, rightKey, score) {
+function Paddle1(x,y,w,h,speed,downKey,upKey) {
   this.x = x;
-    this.y = y;
-    this.vx = 0;
-    this.vy = 0;
-    this.w = w;
-    this.h = h;
-    this.speed = speed;
-    this.downKey = downKey;
-    this.upKey = upKey;
-    /////// NEW //////
-    this.leftKey = leftKey;
-    this.rightKey = rightKey;
-    this.score = score;
+  this.y = y;
+  this.vx = 0;
+  this.vy = 0;
+  this.w = w;
+  this.h = h;
+  this.speed = speed;
+  this.downKey = downKey;
+  this.upKey = upKey;
 }
 
 // handleInput()
@@ -28,36 +24,28 @@ function Paddle1(x, y, w, h, speed, downKey, upKey, leftKey, rightKey, score) {
 // appropriately
 Paddle1.prototype.handleInput = function() {
   if (keyIsDown(this.upKey)) {
-      this.vy = -this.speed;
-    } else if (keyIsDown(this.downKey)) {
-      this.vy = this.speed;
-    }
-    ////// NEW //////
-    else if (keyIsDown(this.leftKey)) {
-      this.vx = -this.speed;
-    } else if (keyIsDown(this.rightKey)) {
-      this.vx = this.speed;
-
-    } else {
-      this.vy = 0;
-      this.vx = 0
-    }
+    this.vy = -this.speed;
   }
+  else if (keyIsDown(this.downKey)) {
+    this.vy = this.speed;
+  }
+  else {
+    this.vy = 0;
+  }
+}
 
 // update()
 // Update y position based on velocity
 // Constrain the resulting position to be within the canvas
 Paddle1.prototype.update = function() {
   this.y += this.vy;
-    this.y = constrain(this.y, 0, height - this.h);
-    this.x += this.vx;
-    this.x = constrain(this.x, 0, width - this.w);
+  this.y = constrain(this.y,0,height-this.h);
 }
 
 // display()
 //
 // Draw the Paddle1 as a rectangle on the screen
 Paddle1.prototype.display = function() {
-  fill(r, g, b);
-    noStroke();
-    rect(this.x, this.y, this.w, this.h);}
+  fill(255);
+  rect(this.x,this.y,this.w,this.h);
+}
